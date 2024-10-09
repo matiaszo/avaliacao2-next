dbe7d75685c5af702197890e7215d9e46f192e55
matiaszo <tiaszuniga@email.com>
2024-10-09 16:05:13 -0300

    arrumar o aparecimento de erros

--- src/app/axios-page/page.tsx
+++ src/app/axios-page/page.tsx
@@ -25,30 +25,36 @@ const axiosPage: React.FC = () => {
 
     useEffect(() => {
     
-        api.get(`/characters/?race=${race}&name=${name}`).then((res) => {
+        api.get(`/characters/?name=${name}`).then((res) => {
             const data = res.data
-
 
             if(name === "" && race === ""){
                 setCharacter(data.items)
-
+                console.log("pri")
             }else if(race === "" && name.length > 0){
                 setCharacter(data)
+                console.log("seg")
             }else if (name === "" && race.length > 0){
                 setCharacter(data)
+                console.log("ter")
             }
             else{
                 setCharacter(data)
+                console.log("else")
             }
 
             setErro(false)
         }).catch((error) => {
-            if(error.response.status === 404){
-                setMessage("Pagina ou personagem nao encontrados")
+            if(error.response.status === 200){
+                setMessage("Personagem nao encontrado")
+                console.log("eroororo")
             }
             if(error.response.staus === 500){
                 setMessage("erro 500")
+            }else{
+                setMessage("Personagem nao encontrado")
             }
+            
             setErro(true)
         })
         
@@ -62,16 +68,16 @@ const axiosPage: React.FC = () => {
         <>
             <div className="flex flex-col items-center justify-center mt-1 gap-1">
                 <div className="flex">
-                    <input type="text" value={name} placeholder="Digite o nome" onChange={(event)=>{setName(event.target.value)}}/>
-                    <input type="text" value={race} placeholder="Digite a raça" onChange={(event)=>{setRace(event.target.value)}}/>
-                    {erro && <h5 className="text-white">{message}</h5>} 
+                    <input type="text" value={name} placeholder="digite o nome" onChange={(event)=>{setName(event.target.value)}}/>
+                    {/* <input type="text" value={race} placeholder="Digite a raça" onChange={(event)=>{setRace(event.target.value)}}/> */}
                  </div>
+                    {erro && <h5 className="text-white">{message}</h5>} 
                 <Suspense fallback={<div>Carregando dados...</div>}>
                     <div className="flex flex-wrap justify-center">
                             {character.map((item, index) => {
                                 return(
-                                    <div className="flex flex-col justify-center items-center text-center border-2">
-                                        <div key={index}>
+                                    <div key={index} className="flex flex-col justify-center items-center text-center border-2">
+                                        <div>
                                             <p>{item.name}</p>
                                             <p>{item.affiliation}</p>
                                             <Image src={item.image} alt="imagem do personagem" className="min-h-80 min-w-80 max-h-80 max-w-80 object-scale-down" width={200} height={200} priority/>
